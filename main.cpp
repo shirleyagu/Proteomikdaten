12f232cabde800b066b5ee9208b52f253816bd5f
shirleyagu <shirley_agu@yahoo.com>
2018-06-12 17:33:32 +0200

    Hello.pro

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    cout<< "Hello World" << endl ;
+
+    return a.exec();
+}
